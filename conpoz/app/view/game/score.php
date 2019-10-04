0455ea5361b13d9555bb228671f794c8f27b3c37
hikaruocean <kenso0012@gmail.com>
2019-10-04 17:09:02 +0800

    score finish

--- conpoz/app/view/game/score.php
+++ conpoz/app/view/game/score.php
@@ -47,21 +47,141 @@ td {
     padding:10px;
 
 }
+.name {
+    width:50%;
+}
+.game {
+    width:25%;
+}
+.score {
+    width:25%;
+}
+.mGame {
+    display: none;
+}
+.mScore {
+    display: none;
+}
 </style>
 <body>
   <table>
       <tr>
-          <td>張 皓綸</td>
-          <td>0</td>
-          <td>0</td>
+          <td class="name p1">張 皓綸</td>
+          <td class="game p1"></td>
+          <td class="score p1"></td>
       </tr>
       <tr>
-          <td>陳 旭紳</td>
-          <td>0</td>
-          <td>0</td>
+          <td class="name p2">陳 旭紳</td>
+          <td class="game p2"></td>
+          <td class="score p2"></td>
       </tr>
   </table>
+  <div class="mGame">
+      <select>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+      </select>
+  </div>
+  <div class="mScore">
+      <select>
+          <option value="0">0</option>
+          <option value="15">15</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="60">AD</option>
+      </select>
+  </div>
   <script src="/lib/jquery/jquery.min.js"></script>
   <script src="/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script>
+  var p1Game = 0;
+  var p2Game = 0;
+  var p1Score = 0;
+  var p2Score = 0;
+      $(function(){
+         updateBoard();
+         $(document).on('click', '.name', function (e) {
+             e.preventDefault();
+             var classStr = $(this).attr('class');
+             var classArray = classStr.split(' ');
+             var player = classArray[1];
+             var vScore = player + 'Score';
+             var vGame = player + 'Game';
+             if (window[vScore] < 30) {
+                 window[vScore] += 15;
+             } else if (window[vScore] == 30) {
+                 window[vScore] += 10;
+             } else if ((p1Score + p2Score) < 80) {
+                 window[vGame] += 1;
+                 scoreReset();
+             } else {
+                 window[vScore] += 20;
+                 if (window[vScore] == 80) {
+                     window[vGame] += 1;
+                     scoreReset();
+                 } else if ((p1Score + p2Score) == 120) {
+                     p1Score = 40;
+                     p2Score = 40;
+                 }
+             }
+             updateBoard();
+         });
+         $(document).on('click', '.game', function () {
+             var classStr = $(this).attr('class');
+             var classArray = classStr.split(' ');
+             var player = classArray[1];
+             var vGame = player + 'Game';
+             $('.mGame select').attr('player', player);
+             $('.mGame select').val(window[vGame]);
+             $('.mGame').toggle();
+         });
+         $(document).on('change', '.mGame select', function () {
+            var vGame = $(this).attr('player') + 'Game';
+            window[vGame] = parseInt($(this).val());
+            updateBoard();
+            $('.mGame').toggle();
+         });
+         $(document).on('click', '.score', function () {
+             var classStr = $(this).attr('class');
+             var classArray = classStr.split(' ');
+             var player = classArray[1];
+             var vScore = player + 'Score';
+             $('.mScore select').attr('player', player);
+             $('.mScore select').val(window[vScore]);
+             $('.mScore').toggle();
+         });
+         $(document).on('change', '.mScore select', function () {
+            var vScore = $(this).attr('player') + 'Score';
+            window[vScore] = parseInt($(this).val());
+            updateBoard();
+            $('.mScore').toggle();
+         });
+     });
+  function scoreReset () {
+      p1Score = 0;
+      p2Score = 0;
+  }
+  function updateBoard () {
+      $('.p1.game').text(p1Game);
+      $('.p2.game').text(p2Game);
+      if (p1Score < 60) {
+          $('.p1.score').text(p1Score);
+      } else {
+          $('.p1.score').text('AD');
+      }
+      if (p2Score < 60) {
+          $('.p2.score').text(p2Score);
+      } else {
+          $('.p2.score').text('AD');
+      }
+  }
+  </script>
 </body>
 </html>
